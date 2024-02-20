a7470e7275818ba52b7acb779d4d5a90163c7055
tomasmota <tomasrebelomota@gmail.com>
2024-02-20 09:20:39 +0100

    move things around

--- broker/broker.go
+++ broker/broker.go
@@ -5,64 +5,26 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
-	"net"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/tomasmota/go-grpc-broker/consumer"
 	pb "github.com/tomasmota/go-grpc-broker/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 type Broker struct {
-	listener   net.Listener
-	grpcServer *grpc.Server
-
-	pb.UnimplementedBrokerServer
-
 	mu        sync.RWMutex
 	consumers map[string]consumer.Consumer
+
+	pb.UnimplementedBrokerServer
 }
 
-func New(port string) *Broker {
+func New() *Broker {
 	broker := &Broker{
 		consumers: make(map[string]consumer.Consumer),
 	}
 
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
-	if err != nil {
-		slog.Error("Failed to create listener: %v", err)
-		os.Exit(1)
-	}
-	broker.listener = listener
-	broker.grpcServer = grpc.NewServer()
-	reflection.Register(broker.grpcServer)
 	return broker
-}
-
-func (b *Broker) Start() {
-	pb.RegisterBrokerServer(b.grpcServer, b)
-
-	go func() {
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-		<-sigChan
-
-		slog.Info("Shutting down server...")
-
-		b.grpcServer.GracefulStop()
-	}()
-
-	slog.Info("Starting server")
-	err := b.grpcServer.Serve(b.listener)
-	if err != nil {
-		slog.Error("Grpc server error: %v", err)
-		os.Exit(1)
-	}
 }
 
 func (b *Broker) Publish(ctx context.Context, pr *pb.PublishRequest) (*pb.Ack, error) {
@@ -108,19 +70,17 @@ func (b *Broker) Subscribe(sr *pb.SubscribeRequest, ss pb.Broker_SubscribeServer
 
 	slog.Info("New consumer added", "name", sr.Consumer.Name)
 
-	go func() {
-		select {
-		case <-finCh:
-			slog.Info("Finishing up consumer", "name", sr.Consumer.Name)
-		case <-ss.Context().Done():
-			slog.Info("Subscriber has disconnected", "name", sr.Consumer.Name)
-		}
+	select {
+	case <-finCh:
+		slog.Info("Finishing up consumer", "name", sr.Consumer.Name)
+	case <-ss.Context().Done():
+		slog.Info("Subscriber has disconnected", "name", sr.Consumer.Name)
+	}
 
-		b.mu.Lock()
-		defer b.mu.Unlock()
-		slog.Info("Removing consumer", "name", sr.Consumer.Name)
-		delete(b.consumers, sr.Consumer.Name)
-	}()
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	slog.Info("Removing consumer", "name", sr.Consumer.Name)
+	delete(b.consumers, sr.Consumer.Name)
 
 	return nil
 }
--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	pb "github.com/tomasmota/go-grpc-broker/proto"
+
 	"github.com/tomasmota/go-grpc-broker/broker"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	broker := broker.New("3030")
-	broker.Start()
+    listener, err := net.Listen("tcp", ":3030")
+	if err != nil {
+		slog.Error("Failed to create listener: %v", err)
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pb.RegisterBrokerServer(grpcServer, broker.New())
+
+	go func() {
+        sigChan := make(chan os.Signal, 1)
+        signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigChan
+
+		slog.Info("Shutting down server...")
+
+		grpcServer.GracefulStop()
+	}()
+
+	slog.Info("Starting server")
+	if err := grpcServer.Serve(listener); err != nil {
+		slog.Error("Grpc server error: %v", err)
+		os.Exit(1)
+    }
 }
